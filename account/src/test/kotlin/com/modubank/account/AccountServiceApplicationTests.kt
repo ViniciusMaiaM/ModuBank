882b4478dcdf9e497bda23a90ea74a8886403762
Vinicius Maia Marinho <viniciusmaiamarinho1@gmail.com>
2026-03-03 11:04:37 -0300

    feat: add comprehensive development infrastructure and tooling

--- account/src/test/kotlin/com/modubank/account/AccountServiceApplicationTests.kt
+++ account/src/test/kotlin/com/modubank/account/AccountServiceApplicationTests.kt
@@ -1,9 +1,12 @@
 package com.modubank.account
 
+import com.modubank.account.integration.config.IntegrationTestConfig
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
+@ContextConfiguration(classes = [IntegrationTestConfig::class])
 class AccountServiceApplicationTests {
     @Test fun contextLoads() {}
 }
